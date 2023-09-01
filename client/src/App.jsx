5c875b29b500b97328225a6dfb733d93827d557e
Kailash Choudhary <kailashjat604@gmail.com>
2023-09-01 22:01:25 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+  </Route>
+))
+
+export default function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
