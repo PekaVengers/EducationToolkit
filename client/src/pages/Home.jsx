60d5eda2de90b34478533a1630ea88c5965a3a2e
singodiyashubham87 <singodiyashubham87@gmail.com>
2023-09-03 15:00:09 +0530

    feat: add studdy buddy link to homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,10 +23,12 @@ export default function Home() {
               <span className="ml-[3rem] text-white text-[2rem]">Video Summarizer</span>
             </button>
            </Link>
-            <button className="flex items-center justify-evenly bg-primary px-[1rem] py-[0.5rem] rounded-[2rem] w-[42%] relative">
+           <Link to={"/study-buddy"}>
+            <button className="flex items-center w-[17.5vw]  justify-evenly bg-primary px-[1rem] py-[0.5rem] rounded-[2rem] relative">
               <div className="circle absolute left-4 w-[40px] h-[40px] rounded-[50%] bg-white"></div>
-              <span className="ml-[1rem] text-white text-[2rem]">Peer Review</span>
+              <span className="ml-[3rem] text-white text-[2rem]">Studdy Buddy </span>
             </button>
+           </Link>            
             <button className="flex items-center justify-evenly bg-primary px-[1rem] py-[0.5rem] rounded-[2rem] w-[42%] relative">
               <div className="circle absolute left-4 w-[40px] h-[40px] rounded-[50%] bg-white"></div>
               <span className="ml-[1rem] text-white text-[2rem]">Not decided</span>
