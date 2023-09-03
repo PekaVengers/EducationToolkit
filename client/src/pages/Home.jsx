78cc0cf90a38bf42cd20e11823733052fafe8a8e
singodiyashubham87 <singodiyashubham87@gmail.com>
2023-09-03 11:20:31 +0530

    patch: homepage video summariser button bug fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,9 +16,9 @@ export default function Home() {
               <span className="ml-[1rem] text-white text-[2rem]">Pdf summariser</span>
             </button>
            <Link to="/videosummary">
-           <button className="flex items-center justify-evenly bg-primary px-[1rem] py-[0.5rem] rounded-[2rem] w-[42%] relative">
+           <button className="flex items-center justify-evenly bg-primary px-[1rem] py-[0.5rem] rounded-[2rem]  relative">
               <div className="circle absolute left-4 w-[40px] h-[40px] rounded-[50%] bg-white"></div>
-              <span className="ml-[2.5rem] text-white text-[2rem]">Video Summariser</span>
+              <span className="ml-[3rem] text-white text-[2rem]">Video Summariser</span>
             </button>
            </Link>
             <button className="flex items-center justify-evenly bg-primary px-[1rem] py-[0.5rem] rounded-[2rem] w-[42%] relative">
