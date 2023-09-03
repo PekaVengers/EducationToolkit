9bca6f7e0638e6e8a7504e31bc7123ad117945a6
singodiyashubham87 <singodiyashubham87@gmail.com>
2023-09-03 16:50:54 +0530

    style: style pdf summariser chat page

--- client/src/pages/PDFSummarizer.jsx
+++ client/src/pages/PDFSummarizer.jsx
@@ -6,11 +6,15 @@ import Sidebar from "../components/Sidebar";
 export async function action({ request }) {
   const formData = await request.formData();
   const fileName = formData.get("source-file").name;
-  const res = await axios.post("http://127.0.0.1:8000/api/pdf-summary/", formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
+  const res = await axios.post(
+    "http://127.0.0.1:8000/api/pdf-summary/",
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
   console.log(typeof res.data);
   return { img: res.data, name: fileName };
 }
@@ -18,70 +22,84 @@ export async function action({ request }) {
 export default function PDFSummarizer() {
   const actionData = useActionData();
   const navigation = useNavigation();
-  const [question, setQuestion] = useState("")
-  const [chatHistory, setChatHistory] = useState([])
+  const [question, setQuestion] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
   async function askQuestion() {
-    const res = await axios.post("http://127.0.0.1:8000/api/ask-question/", { question });
+    const res = await axios.post("http://127.0.0.1:8000/api/ask-question/", {
+      question,
+    });
     setQuestion("");
-    setChatHistory(res.data.history)
-    console.log(res.data.history)
+    setChatHistory(res.data.history);
+    console.log(res.data.history);
   }
 
   function handleChange(event) {
     setQuestion(event.target.value);
   }
 
-  return (<div className="videoContainer flex flex-1 bg-secondary">
-    <Sidebar />
-    <div className="right w-[75vw] flex flex-col justify-center items-center ">
-      {!actionData ? (
-        <div className="container">
-        <form>
-          <input className="border-2 border-black py-2 px-3" type="text" value={question} onChange={handleChange} />
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5" onClick={askQuestion} type="button">Ask</button>
-        </form>
-        
-              <div>
-                <span>User: </span>
-                <span>This is a sample question</span>
-              </div>
-              <div >
-                <span>Bot: </span>
-                <span>This is a sample answer</span>
-              </div>
-      
-      </div>
-      ) : (
-        <div className="rightContent w-[70%] bg-[#88C7E7] px-[4rem] py-[3rem] flex flex-col  rounded-[2rem]">
-          <h1 className="text-[5rem]">PDF Summariser</h1>
-          <h2 className="text-tertiary text-[3rem] mb-8">
-          Upload your PDF and get all your queries regarding that PDF answered by our AI Model.
-          </h2>
-          <Form method="post" encType="multipart/form-data" className="form  flex justify-between items-center" >
-            <div className="selectFile">
-            <input
-              type="file"
-              name="source-file"
-              accept="application/pdf"
-              required
-              className="text-[2rem] "
-            />
+  return (
+    <div className="videoContainer flex flex-1 bg-secondary">
+      <Sidebar />
+      <div className="right w-[75vw] flex flex-col justify-center items-center ">
+        {!actionData ? (
+          <div className="container  w-[80%] flex flex-col ">
+            <form className="flex  w-[100%] mb-[4rem]">
+              <input
+                className="text-[2rem]  py-2 px-3 bg-[#0C2735] text-white placeholder:text-white w-[80%]"
+                placeholder="Ask your query..."
+                type="text"
+                value={question}
+                onChange={handleChange}
+              />
+              <button
+                className="text-[2rem] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5 w-[20%]"
+                onClick={askQuestion}
+                type="button"
+              >
+                Ask
+              </button>
+            </form>
+            <div className="mb-[1rem] ">
+              <span className="text-[2rem] bg-primary rounded px-2 mr-2 text-white">User: </span>
+              <span className="text-[2rem]">This is a sample question which I asked from the artificial intelligence Model and now it's going to provide me with some response</span>
             </div>
-            <button
-              disabled={navigation.state === "submitting"}
-              type="submit"
-              className="text-primary px-8 py-2 hover:bg-primary hover:text-white  bg-white text-[2rem]  border-none rounded-[1rem]"
+            <div >
+              <span className="text-[2rem] bg-black rounded px-2 mr-2 text-white">Bot: </span>
+              <span className="text-[2rem]">This is a sample answer</span>
+            </div>
+          </div>
+        ) : (
+          <div className="rightContent w-[70%] bg-[#88C7E7] px-[4rem] py-[3rem] flex flex-col  rounded-[2rem]">
+            <h1 className="text-[5rem]">PDF Summariser</h1>
+            <h2 className="text-tertiary text-[3rem] mb-8">
+              Upload your PDF and get all your queries regarding that PDF
+              answered by our AI Model.
+            </h2>
+            <Form
+              method="post"
+              encType="multipart/form-data"
+              className="form  flex justify-between items-center"
             >
-              {
-                navigation.state === "submitting"
-                  ? "Processing..."
-                  : "Submit"
-              }
-            </button>
-          </Form>
-        </div>
-      )}
+              <div className="selectFile">
+                <input
+                  type="file"
+                  name="source-file"
+                  accept="application/pdf"
+                  required
+                  className="text-[2rem] "
+                />
+              </div>
+              <button
+                disabled={navigation.state === "submitting"}
+                type="submit"
+                className="text-primary px-8 py-2 hover:bg-primary hover:text-white  bg-white text-[2rem]  border-none rounded-[1rem]"
+              >
+                {navigation.state === "submitting" ? "Processing..." : "Submit"}
+              </button>
+            </Form>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
   );
 }
